570dfbf41e8bda39c9d39fc55055e1a4df26cf54
PatrykF <103563386+70ziko@users.noreply.github.com>
2024-08-11 15:59:46 +0200

    add file reading

--- main.go
+++ main.go
@@ -2,32 +2,34 @@ package main
 
 import (
     "encoding/binary"
+    "errors"
+    "fmt"
+    "io"
     "os"
-    // "fmt"
 )
 
 type FileHeader struct {
-    Magic       [4]byte  // Identifier for the file format, e.g., "NEST"
-    Version     uint16   // Version of the file format
-    Width       uint32   // Width of the main image
-    Height      uint32   // Height of the main image
-    TileSize    uint16   // Size of each tile (e.g., 256x256)
-    NestedCount uint32   // Number of nested images
+    Magic       [4]byte
+    Version     uint16
+    Width       uint32
+    Height      uint32
+    TileSize    uint16
+    NestedCount uint32
 }
 
 type PixeLink struct {
-    R, G, B    byte    // RGB color values
-    NestedIdx  uint32  // Index of the nested image (0 if no nested image)
+    R, G, B   byte
+    NestedIdx uint32
 }
 
 type Tile struct {
-    PixeLinks []PixeLink // PixeLinks in the tile, row by row
+    PixeLinks []PixeLink
 }
 
 type NestedImage struct {
     Width  uint16
     Height uint16
-    Data   []byte // Raw image data (e.g., JPEG or PNG)
+    Data   []byte
 }
 
 const MAGIC = "NEST"
@@ -35,49 +37,45 @@ const MAGIC = "NEST"
 func WriteFile(filename string, mainImage [][]PixeLink, nestedImages []NestedImage) error {
     file, err := os.Create(filename)
     if err != nil {
-        return err
+        return fmt.Errorf("failed to create file: %w", err)
     }
     defer file.Close()
 
+    if len(mainImage) == 0 || len(mainImage[0]) == 0 {
+        return errors.New("main image is empty")
+    }
+
     header := FileHeader{
-        Magic:       [4]byte{'N', 'E', 'S', 'T'},
         Version:     1,
         Width:       uint32(len(mainImage[0])),
         Height:      uint32(len(mainImage)),
         TileSize:    256,
         NestedCount: uint32(len(nestedImages)),
     }
+    copy(header.Magic[:], MAGIC)
 
-    // Write header
-    err = binary.Write(file, binary.LittleEndian, &header)
-    if err != nil {
-        return err
+    if err := binary.Write(file, binary.LittleEndian, &header); err != nil {
+        return fmt.Errorf("failed to write header: %w", err)
     }
 
-    // Write main image data (tiles)
     for y := 0; y < len(mainImage); y += int(header.TileSize) {
         for x := 0; x < len(mainImage[0]); x += int(header.TileSize) {
             tile := extractTile(mainImage, x, y, int(header.TileSize))
-            err = binary.Write(file, binary.LittleEndian, tile)
-            if err != nil {
-                return err
+            if err := binary.Write(file, binary.LittleEndian, tile); err != nil {
+                return fmt.Errorf("failed to write tile at (%d, %d): %w", x, y, err)
             }
         }
     }
 
-    // Write nested images
-    for _, img := range nestedImages {
-        err = binary.Write(file, binary.LittleEndian, img.Width)
-        if err != nil {
-            return err
+    for i, img := range nestedImages {
+        if err := binary.Write(file, binary.LittleEndian, img.Width); err != nil {
+            return fmt.Errorf("failed to write nested image %d width: %w", i, err)
         }
-        err = binary.Write(file, binary.LittleEndian, img.Height)
-        if err != nil {
-            return err
+        if err := binary.Write(file, binary.LittleEndian, img.Height); err != nil {
+            return fmt.Errorf("failed to write nested image %d height: %w", i, err)
         }
-        _, err = file.Write(img.Data)
-        if err != nil {
-            return err
+        if _, err := file.Write(img.Data); err != nil {
+            return fmt.Errorf("failed to write nested image %d data: %w", i, err)
         }
     }
 
@@ -94,5 +92,79 @@ func extractTile(image [][]PixeLink, x, y, size int) []PixeLink {
     return tile
 }
 
-// ReadFile function would be implemented similarly, reading the header first
-// and then the tiles and nested images.
+func ReadFile(filename string) ([][]PixeLink, []NestedImage, error) {
+    file, err := os.Open(filename)
+    if err != nil {
+        return nil, nil, fmt.Errorf("failed to open file: %w", err)
+    }
+    defer file.Close()
+
+    var header FileHeader
+    if err := binary.Read(file, binary.LittleEndian, &header); err != nil {
+        return nil, nil, fmt.Errorf("failed to read header: %w", err)
+    }
+
+    if string(header.Magic[:]) != MAGIC {
+        return nil, nil, errors.New("invalid file format")
+    }
+
+    mainImage := make([][]PixeLink, header.Height)
+    for i := range mainImage {
+        mainImage[i] = make([]PixeLink, header.Width)
+    }
+
+    tileSize := int(header.TileSize)
+    for y := 0; y < int(header.Height); y += tileSize {
+        for x := 0; x < int(header.Width); x += tileSize {
+            tile := make([]PixeLink, tileSize*tileSize)
+            if err := binary.Read(file, binary.LittleEndian, &tile); err != nil {
+                return nil, nil, fmt.Errorf("failed to read tile at (%d, %d): %w", x, y, err)
+            }
+            fillTile(mainImage, tile, x, y, tileSize)
+        }
+    }
+
+    nestedImages := make([]NestedImage, header.NestedCount)
+    for i := range nestedImages {
+        if err := binary.Read(file, binary.LittleEndian, &nestedImages[i].Width); err != nil {
+            return nil, nil, fmt.Errorf("failed to read nested image %d width: %w", i, err)
+        }
+        if err := binary.Read(file, binary.LittleEndian, &nestedImages[i].Height); err != nil {
+            return nil, nil, fmt.Errorf("failed to read nested image %d height: %w", i, err)
+        }
+        nestedImages[i].Data = make([]byte, nestedImages[i].Width*nestedImages[i].Height*3) // Assuming RGB format
+        if _, err := io.ReadFull(file, nestedImages[i].Data); err != nil {
+            return nil, nil, fmt.Errorf("failed to read nested image %d data: %w", i, err)
+        }
+    }
+
+    return mainImage, nestedImages, nil
+}
+
+func fillTile(mainImage [][]PixeLink, tile []PixeLink, x, y, tileSize int) {
+    for j := 0; j < tileSize && y+j < len(mainImage); j++ {
+        for i := 0; i < tileSize && x+i < len(mainImage[y+j]); i++ {
+            mainImage[y+j][x+i] = tile[j*tileSize+i]
+        }
+    }
+}
+
+func main() {
+    mainImage := generateSampleMainImage(1024, 768)
+    nestedImages := generateSampleNestedImages(5)
+
+    err := WriteFile("sample.nest", mainImage, nestedImages)
+    if err != nil {
+        fmt.Printf("Error writing file: %v\n", err)
+        return
+    }
+
+    readMainImage, readNestedImages, err := ReadFile("sample.nest")
+    if err != nil {
+        fmt.Printf("Error reading file: %v\n", err)
+        return
+    }
+
+    fmt.Printf("Read main image dimensions: %dx%d\n", len(readMainImage[0]), len(readMainImage))
+    fmt.Printf("Read %d nested images\n", len(readNestedImages))
+}
